86918988aff5c7c1241dbdbb7a23bea78a2979e8
priyanshu7175 <priyanshuadhikari7@gmail.com>
2025-07-30 22:41:47 -0400

    Annotation Component Scan and Component

---
+++ src/main/java/com/example/annotation/componentScan/App.java
@@ -0,0 +1,15 @@
+package com.example.annotation.componentScan;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+
+        ApplicationContext context =  new ClassPathXmlApplicationContext("applicationComponentScan.xml");
+
+        Employee emp =context.getBean("employee",Employee.class);
+        System.out.println(emp.toString());
+
+    }
+}
